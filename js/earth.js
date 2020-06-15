d0ce1b814d4a514abc24c142b78e0208f455bcd6
Siddharth Nath <sidnath21@gmail.com>
2020-06-14 21:38:12 -0700

    Final Updates

---
+++ js/earth.js
@@ -0,0 +1,212 @@
+let sphere, renderer, camera, scene, stars = [];
+let radius = 30;
+let phi, theta;
+let longitude = [];
+let latitude = [];
+let x = [];
+let y  = [];
+let z = [];
+let x2 = [], y2 = [], z2 = [];
+let numDeaths = [];
+let numRecovered = [];
+let originVector = new THREE.Vector3(0,0,0);
+
+const texture2 = new THREE.TextureLoader().load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_atmos_4096.jpg', () => { renderer.render(scene, camera) })
+texture2.anisotropy = 1; 
+
+function addSphere(){
+
+  
+  for (let z = -1000; z < 1000; z+=20 ) {
+
+    let geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+    let material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+    let sphere = new THREE.Mesh(geometry, material)
+
+    sphere.position.x = Math.random() * 1000 - 500;
+    sphere.position.y = Math.random() * 1000 - 500;
+    sphere.position.z = z;
+
+    sphere.scale.x = sphere.scale.y = 2;
+
+    scene.add( sphere );
+    stars.push(sphere);
+  }
+}
+
+function animateStars() {
+  
+  for(var i=0; i<stars.length; i++) {
+
+    star = stars[i];
+    star.position.z +=  i/10;
+    if(star.position.z>1000) star.position.z-=2000;
+
+  }
+
+}
+
+function init() {
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 60;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  scene.add(earth());
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.autoRotate = true;
+  controls.update();
+
+  const light = new THREE.AmbientLight(0xffffff, 1.0);
+  scene.add(light);
+
+}
+
+function earth(){
+
+  let geometry = new THREE.SphereGeometry(radius, 32, 32);
+
+  const material = new THREE.MeshLambertMaterial({ map: texture2 });
+  sphere = new THREE.Mesh(geometry, material);
+  sphere.position.set(0, 0, 0);
+
+  return sphere;
+
+}
+
+
+function block(){
+
+  let materialA = new THREE.MeshBasicMaterial( {color: 0xC21807, wireframe: false} );
+  let materialB = new THREE.MeshBasicMaterial( {color: 0x008000, wireframe: false} );
+
+  fetch("https://www.trackcorona.live/api/countries")
+      .then(function(response) { return response.json() })
+      .then(function(locations) {
+
+        console.log(locations.data);
+
+        
+        for(let i=0; i<locations.data.length; i++){
+
+          
+          
+          latitude[i] = locations.data[i]["latitude"];
+          longitude[i] = locations.data[i]["longitude"];
+
+          
+          phi = (latitude[i])*Math.PI/180;
+          theta = (longitude[i]-180)*Math.PI/180;
+          
+          numDeaths[i] = locations.data[i]["dead"];
+          numRecovered[i] = locations.data[i]["recovered"];
+
+          
+          heightA = getHeight(numDeaths[i]);
+          
+          heightB = (numRecovered[i] * heightA) / numDeaths[i];
+
+          x[i] = -1 * (radius) * Math.cos(phi) * Math.cos(theta);
+
+          y[i] = (radius) * Math.sin(phi);
+
+          z[i] = (radius) * Math.cos(phi) * Math.sin(theta);
+
+
+
+          x2[i] = -1 * (radius + heightA) * Math.cos(phi) * Math.cos(theta);
+
+          y2[i] = (radius + heightA) * Math.sin(phi);
+
+          z2[i] = (radius + heightA) * Math.cos(phi) * Math.sin(theta);
+
+
+          let geoA = new THREE.BoxGeometry(0.5, 0.5, heightA);
+          let cubeA = new THREE.Mesh( geoA, materialA );
+
+          let geoB = new THREE.BoxGeometry(0.5, 0.5, heightA);
+          let cubeB = new THREE.Mesh( geoB, materialB);
+
+          cubeA.position.set(x[i],y[i],z[i]);
+          cubeA.lookAt(originVector);
+          scene.add(cubeA);
+
+          cubeB.position.set(x2[i], y2[i], z2[i]);
+          cubeB.lookAt(originVector);
+          scene.add(cubeB);
+     
+
+        }
+
+      })
+      .catch(function(){
+        console.log("Location error");
+      })
+
+}
+
+var animate = function () {
+
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+  animateStars();
+
+};
+
+function getHeight(val) {
+  
+  let OldMax = 117853;
+  let OldMin = 0;
+  let NewMax = 10;
+  let NewMin = 1;
+
+  let OldValue = val;
+
+  let OldRange = (OldMax - OldMin);  
+  let NewRange = (NewMax - NewMin);  
+  let NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+  
+  return NewValue;
+
+}
+
+function getDeathHeight(val) {
+  
+  let OldMax = 2162144;
+  let OldMin = 1;
+  let NewMax = 180;
+  let NewMin = 18;
+
+  let OldValue = val;
+
+  let OldRange = (OldMax - OldMin);  
+  let NewRange = (NewMax - NewMin);  
+  let NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+  
+  return NewValue;
+
+}
+
+window.addEventListener("resize", function(){
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  renderer.setSize(w, h);
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
+
+})
+
+
+
+
+
+init();
+addSphere();
+animate();
+block();
---
+++ js/script.js
@@ -0,0 +1,117 @@
+let topic = document.querySelector(".topic");
+let covidData;
+let country;
+let date;
+
+let globalTotalCases, globalTotalDeaths, globalTotalRecovered, dailyGlobalCases, dailyGlobalDeaths, dailyGlobalRecovered;
+
+let totalCases = document.querySelector(".total-cases");
+let totalDeaths = document.querySelector(".total-deaths");
+let totalRecovered = document.querySelector(".total-recovered");
+let dailyCases = document.querySelector(".daily-cases");
+let dailyDeaths = document.querySelector(".daily-deaths");
+let dailyRecovered = document.querySelector(".daily-recovered");
+
+
+let generateData = document.querySelector("#submit");
+let textForm = document.querySelector("#form");
+
+generateData.addEventListener("click", function(){
+  country = textForm.elements["title"].value;
+  if(country.toLowerCase() == "global"){
+    updateTitle("Global");
+    updateNums(globalTotalCases, globalTotalDeaths, dailyGlobalCases, dailyGlobalDeaths);
+  }
+  else{
+    getCountryData(country);
+  }
+});
+
+
+function getCountryData(country){
+
+  let ID = getCountryID(country);
+  if (ID < 0) alert("Choose A Valid Country!");
+  
+  else{
+    
+    let countryTotalCases = covidData[ID]["TotalConfirmed"];
+    let countryTotalDeaths = covidData[ID]["TotalDeaths"];
+    let countryTotalRecovered = covidData[ID]["TotalRecovered"];
+    let countryDailyCases = covidData[ID]["NewConfirmed"];
+    let countryDailyDeaths = covidData[ID]["NewDeaths"];
+    let countryDailyRecovered = covidData[ID]["NewRecovered"];
+
+    updateTitle(covidData[ID].Country);
+    updateNums(countryTotalCases, countryTotalDeaths, countryTotalRecovered , countryDailyCases, countryDailyDeaths, countryDailyRecovered);
+
+  }
+}
+
+function getCountryID(country){
+
+  if(country.toLowerCase() == "usa") return 177;
+  
+  for (let i=0; i<covidData.length; i++){
+    
+    if(country.toLowerCase() == covidData[i].Country.toLowerCase() || country.toLowerCase() == covidData[i].CountryCode.toLowerCase()){
+      return i;
+    }
+    
+  }
+  return -1;
+}
+
+
+function updateTitle(str){
+  topic.innerHTML = str + " | " + date;
+}
+
+function updateNums(totCases, totDeaths, totRecovered, dayCases, dayDeaths, dayRecovered){
+  totalCases.innerHTML = totCases;
+  totalDeaths.innerHTML = totDeaths;
+  totalRecovered.innerHTML = totRecovered;
+  dailyCases.innerHTML = dayCases;
+  dailyDeaths.innerHTML = dayDeaths;
+  dailyRecovered.innerHTML = dayRecovered;
+}
+
+
+function getData(){
+
+  fetch("https://api.covid19api.com/summary")
+  .then(function(response) { return response.json() })
+  .then(function(data) {
+
+
+    let globalData = data["Global"];
+
+    globalTotalCases = globalData["TotalConfirmed"];
+    globalTotalDeaths = globalData["TotalDeaths"];
+    globalTotalRecovered = globalData["TotalRecovered"];
+    dailyGlobalCases = globalData["NewConfirmed"];
+    dailyGlobalDeaths = globalData["NewDeaths"];
+    dailyGlobalRecovered = globalData["NewRecovered"];
+
+    date = data["Date"].substr(0, 10);
+
+    covidData = data["Countries"];
+
+    
+    updateTitle("Global");
+    updateNums(globalTotalCases, globalTotalDeaths, globalTotalRecovered, dailyGlobalCases, dailyGlobalDeaths, dailyGlobalRecovered);
+
+
+  })
+  .catch(function(){
+    console.log("error");
+  })
+
+}
+
+
+
+
+window.onload = function (){ 
+  this.getData(); 
+}
